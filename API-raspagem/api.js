5e00e7cb8dce6a2cdf48c473a5e1b0d9dce13a09
sesaviorangell <sr188bet@gmail.com>
2024-12-11 01:22:30 -0300

    falta agora apenas termina as outras raspagem de noticas e jogos completo ou algo futuro

--- API-raspagem/api.js
+++ API-raspagem/api.js
@@ -11,7 +11,7 @@ const seletores = {
     ContCampenalto:'.Box.klGMtt >.Box.Flex.cBIkhT.jLRkRA',
     nomePais: 'Box.Flex.eHnLBZ.kkrevz > a > .Text.cbiYVh',
     contPaisOuLiga:'.Box.klGMtt >.Box.Flex.cBIkhT.jLRkRA > .Box.Flex.dOBJED.hURKmT',
-    paisOuLiga: '.Box.Flex.cBIkhT.jLRkRA > .Box.Flex.dOBJED.hURKmT a .Text.eOobrs',
+    paisOuLiga: '.Box.Flex.dOBJED.hURKmT .Text.bZlPcX', 
     imgPais: '.Box.Flex.cBIkhT.jLRkRA img.Img.ccYJkt, .Box.Flex.cBIkhT.jLRkRA img.Img.kMzyHA, .Box.Flex.cBIkhT.jLRkRA img.Img.ccYJkt ',
 
     contHorarioTempo:'.Box.jKVshf',
@@ -30,14 +30,15 @@ const seletores = {
 };
 
 async function apiRasp(url) {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-     
+
     await page.goto(url, { timeout: 45000 });
 
-    // Clique ao vivo 
-    const aoVivo = '.sc-52cfa3b2-0.gQokGz';
-    
+    // Clique ao vivo
+    const aoVivo = '.Chip.elpdkn';
+    await page.waitForSelector(aoVivo, { visible: true });
+
     await page.evaluate((selector) => {
         const button = document.querySelector(selector);
         if (button) {
@@ -45,73 +46,80 @@ async function apiRasp(url) {
         }
     }, aoVivo);
     console.log('click ok');
-    
-    // Aguarde o carregamento dos campeonatos
-    await page.waitForSelector(seletores.ContJogos, { timeout:30000 });
-    console.log('Seletor encontrado , cont pais');
-    await page.waitForSelector(seletores.contHorarioTempo, { timeout: 10000 });
-    console.log('Seletor encontrado , horario');
 
-    await page.waitForSelector(seletores.contPlacar, { timeout: 10000 });
-    console.log('Seletor encontrado , placar');
+     // Aguarde o carregamento dos campeonatos
+     await page.waitForSelector(seletores.ContJogos, { timeout:30000 });
+     console.log('Seletor encontrado , cont pais');
+     await page.waitForSelector(seletores.contHorarioTempo, { timeout: 10000 });
+     console.log('Seletor encontrado , horario');
 
-    await page.waitForSelector(seletores.paisOuLiga , { timeout: 10000 });
-    console.log('Seletor encontrado , campenalto ');
-    await page.waitForSelector(seletores.separdor,{timeout: 20000});
+     await page.waitForSelector(seletores.contPlacar, { timeout: 10000 });
+     console.log('Seletor encontrado , placar');
 
-    // Inicializa a variável dados aqui
-    let dados = [];
+     await page.waitForSelector(seletores.paisOuLiga , { timeout: 10000 });
+     console.log('Seletor encontrado , campenalto ');
 
-    const scrollHeight = await page.evaluate(() => {
+     // Inicializa a variável dados aqui
+     let dados = [];
+    const jogosVisibles = new Set([]); // Set para garantir que os jogos não se repitam
+
+
+     const scrollHeight = await page.evaluate(() => {
         return document.body.scrollHeight;
-    });
-    
-    const viewportHeight = await page.evaluate(() => {
-        return window.innerHeight;
-    });
-    
-    const scrollStep = 500; // ajuste esse valor para controlar a velocidade do scroll
-    
-    for (let i = 0; i < scrollHeight; i += scrollStep) {
-        await page.evaluate((scrollHeight) => {
-            window.scrollTo(0, scrollHeight);
-        }, i);
-    
-        await new Promise(resolve => setTimeout(resolve, 1000));
-      
-        const novosDados = await page.evaluate(() => {
-            const campeonatos = [];
-            let campeonatoAtual = null;
-            let indiceCampeonato = 1;
-            let indiceJogo = 1;
+     });
+
+     const viewportHeight = await page.evaluate(() => {
+         return window.innerHeight;
+     });
+
+     const scrollStep = 700; // ajuste esse valor para controlar a velocidade do scroll
+
+     for (let i = 0; i < scrollHeight; i += scrollStep) {
+        // Fazendo scroll progressivamente
+        await page.evaluate(scrollPos => window.scrollTo(0, scrollPos), i);
+        await new Promise(resolve => setTimeout(resolve, 1000)); // Esperando o carregamento do conteúdo
+
+         const novosDados = await page.evaluate(() => {
+             const campeonatos = [];
+             let campeonatoAtual = null;
+             let indiceCampeonato = 1;
+             let indiceJogo = 1;
 
             // Função para extrair informações do jogo
-            const extrairJogo = (elemento) => {
-                const times = elemento.querySelectorAll('.Text.ezSveL');
-                const placar = elemento.querySelectorAll('.Text.cvwZXc.currentScore');
-                const horario = elemento.querySelector('.Box.jKVshf .Text.kcRyBI');
-                const tempo = elemento.querySelector('.Box.Flex.jTiCHC.cRYpNI.sc-efac74ba-2.gxmYGv.score-box .Text.fjeMtb.currentScore bdi');
-                const escudos = elemento.querySelectorAll('.Img.jbaYme');
-                return {
-                    escudoH1:  escudos[0].src || '',
-                    timeHome: times[0]?.textContent.trim() || '',
-                    escudoH2 : escudos[1].src || '',
-                    timeAway: times[1]?.textContent.trim() || '',
-                    placarHome: placar[0]?.textContent.trim() || '0',
-                    placarAway: placar[1]?.textContent.trim() || '0',
-                    horario: horario?.textContent.trim() || '--/--',
-                    tempo: tempo?.textContent.trim() || '0',
-                };
-            };
-
-            // Iterar sobre todos os elementos relevantes
-            document.querySelectorAll('.Box.klGMtt, .HorizontalDivider.gqlAIl')?.forEach((elemento) => {
-                if (elemento.querySelector('.Text.eOobrs')) {
+             const extrairJogo = (elemento) => {
+                 const times = elemento.querySelectorAll('.Text.ezSveL');
+                 const placar = elemento.querySelectorAll('.Text.cvwZXc.currentScore');
+                 const horario = elemento.querySelector('.Box.jKVshf .Text.kcRyBI');
+                 const tempo = elemento.querySelector('.Box.Flex.jTiCHC.cRYpNI.sc-efac74ba-2.gxmYGv.score-box .Text.fjeMtb.currentScore bdi');
+                 const escudos = elemento.querySelectorAll('.Img.jbaYme');
+                 return {
+                     escudoH1:  escudos[0].src || '',
+                     timeHome: times[0]?.textContent.trim() || '',
+                     escudoH2 : escudos[1].src || '',
+                     timeAway: times[1]?.textContent.trim() || '',
+                     placarHome: placar[0]?.textContent.trim() || '0',
+                     placarAway: placar[1]?.textContent.trim() || '0',
+                     horario: horario?.textContent.trim() || '--/--',
+                     tempo: tempo?.textContent.trim() || '0',
+                 };
+             };
+             function divisor (){
+                const divisorCam = document.querySelector('.HorizontalDivider')
+                if (divisorCam){
+                    return divisorCam
+                }else{
+                    return null
+                }
+            } ;
+             document.querySelectorAll('.Box.klGMtt, .gqlAIl', divisor)?.forEach((elemento) => {
+                if (elemento.querySelector('.Text.bZlPcX')) {
                     // Encontrou um novo campeonato
-                    const nomeCampeonato = elemento.querySelector('.Text.eOobrs')?.textContent.trim();
+                    const nomeCampeonato = elemento.querySelector('.Text.bZlPcX')?.textContent.trim();
                     const imgP = elemento.querySelector('.Box.Flex.cBIkhT.jLRkRA img.Img.ccYJkt, .Box.Flex.cBIkhT.jLRkRA img.Img.kMzyHA, .Box.Flex.cBIkhT.jLRkRA img.Img.ccYJkt ');
-                    const imgPSrc = imgP? imgP.src : '' ;
-                    if (nomeCampeonato) {
+                    const imgPSrc = imgP ? imgP.src : '';
+            
+                    // Verifica se o campeonato já foi adicionado
+                    if (nomeCampeonato && !campeonatos.some(c => c.nome === nomeCampeonato)) {
                         // Finaliza o campeonato atual e inicia um novo
                         if (campeonatoAtual) {
                             campeonatos.push(campeonatoAtual);
@@ -131,41 +139,95 @@ async function apiRasp(url) {
                         const horario = elemento.querySelector('.Box.jKVshf .Text.kcRyBI')?.textContent.trim();
                         const tempo = elemento.querySelector('.Box.Flex.jTiCHC.cRYpNI.sc-efac74ba-2.gxmYGv.score-box .Text.fjeMtb.currentScore bdi')?.textContent.trim();
                         const jogoInfo = extrairJogo(jogo);
-                        if (campeonatoAtual) {
-                            const id = campeonatoAtual.indice + "." + indiceJogo;
+                        const id = campeonatoAtual ? campeonatoAtual.indice + "." + indiceJogo : '';
+            
+                        // Verifica se o jogo já foi adicionado
+                        if (campeonatoAtual && !campeonatoAtual.jogos.some(j => j.id === id)) {
                             indiceJogo++;
                             return { ...jogoInfo, horario, tempo, id };
                         } else {
-                            return { ...jogoInfo, horario, tempo, id: '' };
+                            return null; // Retorna null se o jogo já existe
                         }
-                    });
-
+                    }).filter(jogo => jogo !== null); // Filtra os jogos nulos
+            
                     if (campeonatoAtual) {
                         campeonatoAtual.jogos.push(...jogos);
-                    } 
+                    }
                 } else if (elemento.classList.contains('gqlAIl') && campeonatoAtual) {
                     // Encontrou um divisor de campeonatos
                     campeonatos.push(campeonatoAtual);
                     campeonatoAtual = null;
                 }
             });
-
+            
             // Adiciona o último campeonato se ainda estiver ativo
             if (campeonatoAtual) {
                 campeonatos.push(campeonatoAtual);
             }
-
+            
             return campeonatos;
-        });
+         });
 
-        // Adiciona novos dados à variável dados
-        dados.push(...novosDados);
-    }
-    
-    console.log(JSON.stringify(dados, null, 2));
-    fs.writeFileSync('dados_sofascore.json', JSON.stringify(dados, null, 2));
-    await browser.close();
+         // Adiciona novos dados à variável dados
+         dados.push(...novosDados);
+     }
+
+    //  console.log(JSON.stringify(dados, null, 2));
+     fs.writeFileSync('dados_sofascore.json', JSON.stringify(dados, null, 2));
+     await browser.close();
 }
 
-// Chamada da função 
+// Chamada da função
 apiRasp(url);
+async function leitura (){
+const caminhoArquivo = './dados_sofascore.json';
+function leDados(caminhoArquivo) {
+    const dadosLido =  fs.readFileSync(caminhoArquivo, 'utf8');
+    return JSON.parse(dadosLido);
+ }
+
+// const dados = leDados(caminhoArquivo);
+// console.log(dados);
+// fs.writeFileSync('dadosLeitura5.json', JSON.stringify(dados, null, 2));
+const organizarDadosCampeonalto = await leDados(caminhoArquivo)
+const dadosMapeados = new Set(organizarDadosCampeonalto.map(item => item.jogos && item.jogos.length > 0 ?  JSON.stringify(item): null ).filter(item => item !== null));
+const dadosSimplificados = [...dadosMapeados].map(item =>JSON.parse(item));
+
+console.log(dadosSimplificados);
+
+// Escreve os dados processados em um novo arquivo
+fs.writeFileSync('dadosMap1.JSON', JSON.stringify(dadosSimplificados, null, 2));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function main(){
+    console.log("Iniciando raspagem...");
+    await apiRasp(url); // Aguarda a conclusão da raspagem
+    console.log("Raspagem concluída! Iniciando leitura...");
+    await leitura(); // Somente agora realiza a leitura
+};
+main();
--- API-raspagem/api2adptacao.js
+++ API-raspagem/api2adptacao.js
@@ -1,41 +1,25 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
 const puppeteer = require('puppeteer');
 
-const url = "https://www.sofascore.com/";
+const url = "https://www.sofascore.com/"; // URL do site para raspagem
 
 const seletores = {
-    contgeral:'.Box.klGMtt ',
-    separdor:'.HorizontalDivider.gqlAIl',
-    ContCampenalto:'.Box.klGMtt >.Box.Flex.cBIkhT.jLRkRA',
+    contgeral: '.Box.klGMtt ',
+    separdor: '.HorizontalDivider.gqlAIl',
+    ContCampenalto: '.Box.klGMtt >.Box.Flex.cBIkhT.jLRkRA',
     nomePais: 'Box.Flex.eHnLBZ.kkrevz > a > .Text.cbiYVh',
-    contPaisOuLiga:'.Box.klGMtt >.Box.Flex.cBIkhT.jLRkRA > .Box.Flex.dOBJED.hURKmT',
-    paisOuLiga: '.Box.Flex.cBIkhT.jLRkRA > .Box.Flex.dOBJED.hURKmT a .Text.eOobrs',
+    contPaisOuLiga: '.Box.klGMtt >.Box.Flex.cBIkhT.jLRkRA > .Box.Flex.dOBJED.hURKmT',
+    paisOuLiga: '.Box.Flex.dOBJED.hURKmT .Text.bZlPcX', // Seletor ajustado
     imgPais: '.Box.Flex.cBIkhT.jLRkRA img.Img.ccYJkt, .Box.Flex.cBIkhT.jLRkRA img.Img.kMzyHA, .Box.Flex.cBIkhT.jLRkRA img.Img.ccYJkt ',
-
-    contHorarioTempo:'.Box.jKVshf',
-    horario: '.Text.kcRyBI',
-    tempo: '.Box.Flex.jTiCHC.cRYpNI.sc-efac74ba-2.gxmYGv.score-box .Text.fjeMtb.currentScore bdi',
-
-    ContJogos:'.Box.dtLxRI',
-    nomeH1: '[data-testid="left_team"] .Text.ezSveL',
-    escudoH1: '[data-testid="left_team"] .Img.jbaYme',
-    nomeH2: '[data-testid="right_team"] .Text.ezSveL',
-    escudoH2: '[data-testid="right_team"] .Img.jbaYme',
-
-    contPlacar: '.Box.Flex.gulcjH.yaNbA',
-    placarH1: '[data-testid="left_score"] .Text.cvwZXc.currentScore',
-    placarH2: '[data-testid="right_score"] .Text.cvwZXc.currentScore'
 };
 
 async function apiRasp(url) {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto(url, { timeout: 45000 });
+
+    await page.goto(url, { timeout: 60000 });
 
     // Clique ao vivo 
-    const aoVivo = '.sc-52cfa3b2-0.gQokGz';
+    const aoVivo = '.Chip.elpdkn';
     
     await page.evaluate((selector) => {
         const button = document.querySelector(selector);
@@ -45,131 +29,28 @@ async function apiRasp(url) {
     }, aoVivo);
     console.log('click ok')
 
-     //Aguarde o carregamento dos campeonatos
-    await page.waitForSelector(seletores.ContJogos, { timeout:30000 });
-    console.log('Seletor encontrado , cont pais');
-    await page.waitForSelector(seletores.contHorarioTempo, { timeout: 10000 });
-    console.log('Seletor encontrado , horario');
+    // Aguardar o carregamento completo da página, especialmente se houver conteúdo dinâmico
 
-    await page.waitForSelector(seletores.contPlacar, { timeout: 10000 });
-    console.log('Seletor encontrado , placar');
-
-    await page.waitForSelector(seletores.paisOuLiga , { timeout: 10000 });
-    console.log('Seletor encontrado , campenalto ');
-    await page.waitForSelector(seletores.separdor,{timeout: 20000})
-
-
-    //   // Função para rolar a página para carregar todo o conteúdo
-    //   const scrollToBottom = async () => {
-    //       await page.evaluate(async () => {
-    //           await new Promise((resolve) => {
-    //              let totalHeight = 0;
-    //               const distance = 550;
-    //               const timer = setInterval(() => {
-    //                   const scrollHeight = document.body.scrollHeight;
-    //                   window.scrollBy(0, distance);
-    //                   totalHeight += distance;
-
-    //                   if (totalHeight >= scrollHeight) {
-    //                       clearInterval(timer);
-    //                       resolve();
-    //                  }
-    //               }, 1000);
-    //           });
-    //       });
-    //  };
-
-    //   await scrollToBottom(); // Rolagem para carregar todos os jogos
-    //  console.log('rolagem finalizada');
-
-    const mostra = await page.evaluate(async (seletores) => {
-        const ligas = [];
+    // Captura os nomes das ligas
+    const ligas = await page.evaluate((seletores) => {
         const ligasElems = document.querySelectorAll(seletores.paisOuLiga);
-        
-        for (const liga of ligasElems) {
-            const campeonatos = []; // Correção aqui
+        const nomesLigas = [];
 
-            const campenaltosElems = liga.closest('.Box.Flex.cBIkhT.jLRkRA').querySelectorAll('.Box.Flex.dOBJED.hURKmT');
-            for (const campenalto of campenaltosElems) {
-                const nomeLiga = campenalto.querySelector(seletores.paisOuLiga)?.textContent.trim(); // Corrigido
-                const imgP = [...liga.closest('.Box.Flex.cBIkhT.jLRkRA').querySelectorAll(seletores.imgPais)].map(img => {
-                    let src = img.getAttribute('src');
-                    if (src.startsWith('/static/images/flags/')) {
-                        src = 'https://www.sofascore.com' + src;
-                    }
-                    return src;
-                });
-
-                campeonatos.push({ // Adiciona ao array campeonatos
-                    nome: nomeLiga,
-                    imgP: imgP
-                });
+        ligasElems.forEach((liga) => {
+            const nomeLiga = liga.textContent.trim();
+            if (nomeLiga) {
+                nomesLigas.push(nomeLiga);  // Adiciona o nome da liga ao array
             }
+        });
 
-            //Verifica se o separador é o esperado
-             if (separador ) {
-                 break; // Se o separador estiver presente, sai do loop para começar o próximo campeonato
-             }
-             const horaTempo = [...document.querySelectorAll(seletores.contHorarioTempo)].map(ht => {
-                 const horarioElem = ht.querySelector(seletores.horario);
-                 const tempoElem = ht.querySelector(seletores.tempo);
-                 return {
-                     hora_tempo: {
-                         horario: horarioElem ? horarioElem.textContent.trim() : "vazio",
-                         tempo: tempoElem ? tempoElem.textContent.trim() : "vazio"
-                     }
-                 };
-            });
-
-            const jogos = [...document.querySelectorAll(seletores.ContJogos)].map(jog => {
-                const nomeH1Elem = jog.querySelector(seletores.nomeH1);
-                const nomeH2Elem = jog.querySelector(seletores.nomeH2);
-                const escudoH1Elem = jog.querySelector(seletores.escudoH1);
-                 const escudoH2Elem = jog.querySelector(seletores.escudoH2);
-
-                 return {
-                     times: {
-                         escudoH1: escudoH1Elem ? escudoH1Elem.getAttribute('src') : "vazio",
-                        nomeH1: nomeH1Elem ? nomeH1Elem.textContent.trim() : "vazio",
-                        x: "=",
-                         escudoH2: escudoH2Elem ? escudoH2Elem.getAttribute('src') : "vazio",
-                         nomeH2: nomeH2Elem ? nomeH2Elem.textContent.trim() : "vazio"
-                     }
-                 };
-             });
-
-             const placarJogos = [...document.querySelectorAll(seletores.contPlacar)].map(placa => {
-                 const placarH1Elems = placa.querySelector(seletores.placarH1);
-                 const placarH2Elems = placa.querySelector(seletores.placarH2);
-                 return {
-                     placar: {
-                         h1: placarH1Elems ? placarH1Elems.textContent.trim() : "vazio",
-                         x: 'x',
-                         h2: placarH2Elems ? placarH2Elems.textContent.trim() : "vazio"
-                     }
-                 };
-             });
-
-             
-
-            ligas.push({
-                campeonatos: campeonatos,
-                
-                 jogos: jogos.map((jogo, index) => ({
-                     ...jogo,
-                      horaTempo: horaTempo,
-                  placar: placarJogos[index] ? placarJogos[index].placar : {}
-                 }))
-            });
-        }
-
-        return ligas;
+        return nomesLigas;
     }, seletores);
-    console.log(JSON.stringify(mostra, null, 2));
 
-    fs.writeFileSync('dados.json', JSON.stringify(mostra, null, 2), 'utf8');
-      browser.close();
+    console.log('Ligas encontradas:', ligas);
+
+    // Fechar o navegador
+    await browser.close();
 }
-// Chamada da função 
+
+// Chamar a função para rodar o processo de raspagem
 apiRasp(url);
-//aqui funcuina perfeito 
