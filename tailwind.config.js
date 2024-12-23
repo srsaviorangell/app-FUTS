03a7f53f42610d9409a15879023add3004e461a9
sesaviorangell <sr188bet@gmail.com>
2024-12-23 05:45:11 -0300

    falta um pouco mais ta tomando forma

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.handlebars",],
+  content: ["./views/**/*.handlebars",
+    "./views/**/*.html",
+  ],
   theme: {
     extend: {
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+      },
       colors: {
-        colorapp: '#433d52',
+        colorapp: '#333030',
+        cormenu: '#403C3C',
+        corletreiro:'#404040',
      },
+     borderRadius: {
+      'custom': '1.25rem', 
+    },
+    boxShadow: {
+      'inner-custom': 'inset 1px 4px 10px 0px rgba(0, 0, 0, 1)', // Adiciona sua sombra interna personalizada
+    },
+    
     },
   },
   plugins: [],
