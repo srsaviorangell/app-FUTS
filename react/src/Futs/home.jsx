3605ed7be702c97c081742083faab00f04d9539e
savio rangel souza costa <sr188bet@gmail.com>
2025-09-03 00:18:56 -0300

    oias

--- react/src/Futs/home.jsx
+++ react/src/Futs/home.jsx
@@ -16,10 +16,15 @@ export function BlocoMeio() {
     <>
       <div className=" border border-white h-[90%] w-full grid grid-cols-12 grid-rows-12 gap-1">
         <div  className=" text-white border col-start-1 col-span-2 ">esquerdo</div>
-        <div  className=" text-white flex justify-center border col-start-3 col-span-7 row-span-12 gap-3 p-2">
-        <LiveGameCard />
-        <LiveGameCard />
-        <LiveGameCard />
+        <div  className=" overflow-y:auto text-white grid  col-start-3 col-span-7 row-span-12 gap-3 p-2">
+          <div className=" border-5  flex justify-center">
+            <LiveGameCard />
+            <LiveGameCard />
+            <LiveGameCard />
+            
+          </div>
+         
+        
 
 
 
